4d4510b8a5aba6f51aed3f6f21421a705fc6d071
Abdul-Rahman Ammourah <abdulrhmanammourah@gmail.com>
2024-06-27 00:04:33 +0300

    even more stuff

---
+++ Back End/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+const string connectionUri = "mongodb+srv://Ammourah:v2Kt5l2EdZlMf6vZ@wannachat.tgwjbnf.mongodb.net/?appName=WannaChat";
+
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+try {
+  var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+  Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (Exception ex) {
+  Console.WriteLine(ex);
+}
