220ecbe743da35e3e2bff63928c4f3f13c98077e
Abdul-Rahman-Ammourah <abdulrhmanammourah@gmail.com>
2024-08-10 16:45:02 +0300

    stuff

--- FrontEnd/Pages/api.js
+++ FrontEnd/Pages/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://wanna-chat.fly.dev', // Your local IP address
+  baseURL: 'http://192.168.1.161:5291', // Your local IP address
   timeout: 10000,  // Optional: Set a timeout for requests
 });
 
@@ -25,22 +25,3 @@ export const login = async (email, password) => {
   }
 };
 
-export const sendMessage = async (from, to, content) => {
-  try {
-    const response = await api.post('/send', { from, to, content });
-    return response.data;
-  } catch (error) {
-    console.error('Send Message Error:', error.message);
-    throw error;
-  }
-};
-
-export const getMessages = async (from, to) => {
-  try {
-    const response = await api.get('/messages', { params: { from, to } });
-    return response.data;
-  } catch (error) {
-    console.error('Get Messages Error:', error.message);
-    throw error;
-  }
-};
