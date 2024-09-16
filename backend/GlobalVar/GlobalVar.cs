99041a8ea3d29a0c2bfba52e6216763170dbed57
lilcooler <129325480+Abdul-Rahman-Ammourah@users.noreply.github.com>
2024-09-16 13:23:34 +0300

    Update GlobalVar.cs

--- backend/GlobalVar/GlobalVar.cs
+++ backend/GlobalVar/GlobalVar.cs
@@ -5,7 +5,7 @@ namespace backend.GlobalVar
     public static class GlobalVariables
     {
         public static readonly MongoClient DbClient = new MongoClient
-        ("mongodb+srv://ammourah:qME6hIWdjgpzBnoU@users.frbjxvq.mongodb.net/?retryWrites=true&w=majority&appName=Users");
+        ("xxx");
     }
 
 }
